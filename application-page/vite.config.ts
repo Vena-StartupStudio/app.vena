8b97eec6c0a1a3361b1673adcbb61057aafb1df0
Shay Levy <shay.levy01@post.runi.ac.il>
2025-09-18 11:40:44 +0300

    fix sign in page 4

--- application-page/vite.config.ts
+++ application-page/vite.config.ts
@@ -1,21 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3001'
-    }
-
-  },
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        signin: resolve(__dirname, 'signin.html'),
-        dashboard: resolve(__dirname, 'dashboard.html')
+        main: './index.html',
+        dashboard: './dashboard.html'
       }
     }
   }
